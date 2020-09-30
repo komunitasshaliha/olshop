dc954ec394d35b2d94b28417b754845a8d703cf5
komunitasshaliha <71205708+komunitasshaliha@users.noreply.github.com>
2020-09-30 22:59:32 +0700

    Update twLSN.js

--- twLSN.js
+++ twLSN.js
@@ -5,6 +5,10 @@ var twLSN = [
 'ganiafrozenfood.my.id',
 'brkhcustom.blogspot.com',
 'brkhcustom.my.id',
+'ibeautystore88.blogspot.com',
+'ibeautystore.my.id',
+'sucitastore.blogspot.com',
+'sucitastore.my.id',
 ];
 
 var redirectUrl = 'http://wa.me/6289644757756';
